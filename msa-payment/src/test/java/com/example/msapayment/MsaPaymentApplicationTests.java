0254f0a4d59655d89c7e41bb3368aaa6d9ba8f96
kklck <asdf5574@naver.com>
2021-07-18 05:12:56 +0900

    [add] 청약 결제 서비스 생성

---
+++ msa-payment/src/test/java/com/example/msapayment/MsaPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msapayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaPaymentApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
